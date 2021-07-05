ab094b6dd1be8476e07be5560c754ca9c115f4a0
Petr <spqr86.sr@gmail.com>
2021-07-05 11:14:11 +0300

    infra

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 from django.contrib.flatpages import views
 from django.urls import include, path
 
-handler404 = "views.page_not_found"  # noqa
-handler500 = "views.server_error"  # noqa
+handler404 = "foodgram.views.page_not_found"  # noqa
+handler500 = "foodgram.views.server_error"  # noqa
 
 urlpatterns = [
     path('admin/', admin.site.urls),
