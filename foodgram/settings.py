578a60799b8dbebe3b91394fb17ea66a1f8ed2e4
Petr <spqr86.sr@gmail.com>
2021-07-09 15:38:09 +0300

    infra

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -122,7 +122,7 @@ LOGIN_REDIRECT_URL = 'index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
